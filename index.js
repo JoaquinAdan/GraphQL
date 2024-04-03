e18b880c9ecb02764846031748c389413612b606
JoaquinAdan <rodriguezjdev@gmail.com>
2024-04-03 14:56:57 -0300

    Add ApolloServer import and update GraphQL schema

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-server'
+import { ApolloServer, gql } from 'apollo-server'
 
 const persons = [
   {
@@ -6,21 +6,21 @@ const persons = [
     phone: '123456789',
     street: 'Calle Falsa 123',
     city: 'Springfield',
-    id: '1',
+    id: '550e8400-e29b-41d4-a716-346655440000',
   },
   {
     name: 'Juan',
     phone: '987654321',
     street: 'Calle Falsa 321',
     city: 'Springfield',
-    id: '2',
+    id: '550e8400-e29b-41d4-a716-246655440000',
   },
   {
     name: 'Pedro',
     phone: '456789123',
     street: 'Calle Falsa 213',
     city: 'Ibiza',
-    id: '3',
+    id: '550e8400-e29b-41d4-a716-146655440000',
   },
 ]
 
@@ -32,4 +32,26 @@ const typeDefs = gql`
     city: String!
     id: ID!
   }
+  type Query {
+    personCount: Int!
+    allPersons: [Person]!
+    findPerson(name: String!): Person
+  }
 `
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
